78ee5b860abbcc8261fbb914b834c27cf9763684
josricor <josricor@outlook.com>
2021-09-12 15:14:39 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -4,34 +4,22 @@ import { useHead } from '@vueuse/head'
 // https://github.com/vueuse/head
 // you can use this to manipulate the document head in any components,
 // they will be rendered correctly in the html results with vite-ssg
-const title = ref('Sricor')
+const title = ref("Sricor's Portfolio")
+const router = useRouter()
+
+// 路由回到顶部
+router.afterEach((to,from,next) => {
+ window.scrollTo(0,0);
+})
 
 useHead({
   title: title.value,
   meta: [
-    { name: 'description', content: 'Opinionated Vite Starter Template' },
+    { name: 'description', content: 'Sricor Portfolio' },
   ],
 })
 </script>
 
-<template>
-  <router-view />
+<template lang="pug">
+router-view
 </template>
-
-<style>
-  :root {
-    --primary-color: #28a386;
-    --font-family: "Inter UI", system-ui, sans-serif
-  }
-
-  body {
-    color: #333;
-    background-color: #fff;
-    letter-spacing: -.1px;
-    font-weight: 400;
-    font-kerning: normal;
-    -moz-font-feature-settings: "kern"1;
-    -webkit-font-feature-settings: "kern"1;
-    font-feature-settings: "kern"1
-  }
-</style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -10,27 +10,32 @@ const toggleLocales = () => {
 }
 
 const navList = [
-  { name: '🎨Designs', link: '/designs' },
-  { name: '🤓About', link: '/about' },
-  { name: '📝Blog', link: 'https://blog.sricor.work', target: '_blank' },
+  { name: '🎨Designs', link: 'designs' },
+  { name: '🤓About', link: 'about' }
 ]
 
+const router = useRouter()
+const go = (name: string) => {
+  router.push(`/${name}`)
+}
 </script>
 
 <template lang="pug">
 nav.main-nav.nav
   .container
     .nav-left
-      a(href="/")
+      a(@click="go('')")
         | Sricor's portfolio
     .nav-right.nav-menu
-      a(href="/")
+      a(@click="go('')")
         svg.feather.feather-home(xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewbox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round")
           path(d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z")
           polyline(points="9 22 9 12 15 12 15 22")
         | {{ t('button.home') }}
-      a(v-for="nav in navList" :key="nav.name" :href="nav.link")
+      a(v-for="nav in navList" :key="nav.name" @click="go(nav.link)")
         | {{ nav.name }}
+      a(href="https://blog.sricor.work", target="_blank")
+        | 📝Blog
       a()
         carbon-sun
       a(@click="toggleLocales")
@@ -45,7 +50,9 @@ nav.main-nav.nav
 .nav-menu {
   flex: 1 1 auto
 }
-
+.nav a {
+  cursor: pointer;
+}
 .logo svg {
   width: 5rem;
   fill: var(--primary-color)
