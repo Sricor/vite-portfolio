1e1a303dfac6bae25f4f8a33ca91b825ae20d469
josricor <josricor@outlook.com>
2021-10-10 16:21:23 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
 import App from './App.vue'
 import { createApp } from 'vue'
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
 // pages
 import Home from './pages/Home.vue'
-import NotFound from './pages/NotFound.vue'
+//import NotFound from './pages/NotFound.vue'
 import VentHub from './pages/VentHub.vue'
 
 // https://next.router.vuejs.org/
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes: [
     {path: '/', name:'Portfolio', component: Home},
-    {path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound},
+    {path: '/:pathMatch(.*)*', name: 'Home', component: Home},
     {path: '/portfolio/ventHub', name:'VentHub', component: VentHub},
   ],
 })
